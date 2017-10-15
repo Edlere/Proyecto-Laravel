a8c91a01c1f4f2a567ef29a3192f3987ec253deb
Jose <josepanta63@gmail.com>
2017-10-15 05:40:53 -0500

    1

--- adminentrust/resources/views/vendor/adminlte/views/carritoCompra.blade.php
+++ adminentrust/resources/views/vendor/adminlte/views/carritoCompra.blade.php
@@ -14,6 +14,10 @@ Libros
 @endsection
 @section('main-content')
 <form method="POST" action="{{url('admin/Carrito')}}" accept-charset="UTF-8" class="" id="GuardarCarrito">
-
+	<div class="row">
+		<div class="col-xs-12">
+			<button type="submit" id="btnGuardarCarrito" class="btn btn-block pull-left btn-success"><i class="fa fa-btc" aria-hidden="true"></i><span style="font-size:20px;">&nbsp;Comprar</span> <i class="fa fa-btc" aria-hidden="true"></i></button>
+		</div>
+	</div>
 </form>
 @endsection
